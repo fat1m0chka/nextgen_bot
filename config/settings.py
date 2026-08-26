b74205e74bf3b96ce065f10bb0136400566e7962
fat1m0chka <155376421+fat1m0chka@users.noreply.github.com>
2026-08-26 23:31:15 +0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     BOT_TOKEN: str
     CHANNEL_ID: str
+    CHANNEL_USERNAME: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
